de3df3f64f0d8195398ea3ed8dfa58617d5cf50a
Manjunath CV <cvmanjoo@users.noreply.github.com>
2019-03-28 20:51:02 +0530

    Revert "Delete PCF8563.h"
    This reverts commit 509578d8424eae34adc84d822f694985dcb69b14.

---
+++ src/PCF8563.h
@@ -0,0 +1,83 @@
+#ifndef PCF8563_H
+#define PCF8563_H
+
+#include <time.h>
+#include <Arduino.h>
+#include <Wire.h>
+
+#define PCF8563_ADDR 0x51
+
+#define SQW001Hz 10
+#define SQW04kHz 14
+#define SQW08kHz 18
+#define SQW32kHz 32
+
+class PCF8563
+{
+    public:
+        bool begin();
+
+        bool isRunning(void);
+        void startClock();
+        void stopClock();
+
+        void setSeconds(uint8_t second);
+        void setMinutes(uint8_t minute);
+        void setHours(uint8_t hour);
+        void setDay(uint8_t day);
+        void setWeek(uint8_t week);
+        void setMonth(uint8_t month);
+        void setYear(uint16_t year);
+
+        void setDate(uint8_t day, uint8_t month, uint16_t year);
+        void setTime(uint8_t hour, uint8_t minute, uint8_t second);
+
+
+        void setDateTime(char* date, char* time);
+
+        uint8_t getSeconds();
+        uint8_t getMinutes();
+        uint8_t getHours();
+        uint8_t getDay();
+        uint8_t getWeek();
+        uint8_t getMonth();
+        uint16_t getYear();
+
+
+        void setEpoch(time_t epoch);
+        time_t getEpoch();
+        //void outPin(uint8_t mode);
+
+        //void enableAlarm();
+        void disableAlarm();
+        void clearAlarm();
+
+        bool isAlarmEnabled();
+        bool isAlarmTriggered();
+
+        void setAlarmHours(uint8_t hours);
+        void setAlarmMinutes(uint8_t minutes);
+        //setAlarmTime();
+        //setAlarmDay();
+
+        uint8_t getAlarmHours();
+        uint8_t getAlarmMinutes();
+        //getAlarmTime();
+        //getAlarmDay();
+
+        //void setOutPin(uint8_t mode);
+        //bool isOutPinEnabled();
+        //bool isSqweEnabled();
+
+    private:
+        uint8_t bin2bcd (uint8_t val);
+        uint8_t bcd2bin (uint8_t val);
+};
+
+
+
+//Instantiate class PCF8563 as RTC
+
+static PCF8563 RTC;
+
+#endif /* PCF8563_H */
